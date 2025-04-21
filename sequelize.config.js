b488ca4a679707d5e13ee99787bbf2d3b317f0f2
Brinton Fernando G <remafdo181299@gmail.com>
2025-04-21 18:15:02 +0530

    modified sequalize config to read env from config

--- sequelize.config.js
+++ sequelize.config.js
@@ -3,4 +3,13 @@ require('dotenv').config();
 
 const config = require('./src/config/config.ts').default;
 
-module.exports = config;
+module.exports = {
+  development: {
+    username: config.database.username,
+    password: config.database.password,
+    database: config.database.name,
+    host: config.database.host,
+    port: config.database.port,
+    dialect: config.database.dialect,
+  }
+};
