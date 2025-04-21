1ab4f17295134f7a22104eb0bdfd9c74aafdbbba
Brinton Fernando G <remafdo181299@gmail.com>
2025-04-21 18:14:14 +0530

    modified port to read from config file

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,11 @@ import express from "express";
 import routes from "./routes/route";
 import config from "./config/config";
 
-const env = "development"
-const dbConfig = config[env];
-
 const app = express();
 app.use(express.json());
+app.use("/api", routes);
 
-app.use("/", routes);
-const PORT =dbConfig.port || 3000;
+const PORT = config.database.port;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
