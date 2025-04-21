5237baa2dd6f32832e120f235ea403a45628eb10
Brinton Fernando G <remafdo181299@gmail.com>
2025-04-21 18:13:04 +0530

    added wrapper for routes

---
+++ src/utils/wrapController.ts
@@ -0,0 +1,6 @@
+// export const wrapController = (fn: Function) => {
+//     return (req: any, res: any, next: any) => {
+//       Promise.resolve(fn(req, res, next)).catch(next);
+//     };
+//   };
+  
